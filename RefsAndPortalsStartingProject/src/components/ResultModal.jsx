a9fa7d23cb0c2802aa1361518c7ac28921c8ae90
Brent Muhlestein <brent.muhlestein@gmail.com>
2025-04-21 20:57:14 -0600

    Course progress after Section 8 video #147.
    Introducing & Understanding "Portals"

--- RefsAndPortalsStartingProject/src/components/ResultModal.jsx
+++ RefsAndPortalsStartingProject/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { useRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
 
 export default function ResultModal({ ref, targetTime, timeRemaining, onReset }) {
     const dialog = useRef();
@@ -13,7 +14,7 @@ export default function ResultModal({ ref, targetTime, timeRemaining, onReset })
         }
     });
     
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your score: {score}</h2>}
@@ -22,6 +23,7 @@ export default function ResultModal({ ref, targetTime, timeRemaining, onReset })
             <form method="dialog" onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     );
 }
