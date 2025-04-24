02b324b469eedf74c22cf3775169aa5806d76745
Brent Muhlestein <brent.muhlestein@gmail.com>
2025-04-24 17:02:06 -0600

    Course progress after Section 9 video #150.
    Initial project + styled sidebar

---
+++ ProjectManagementProject/src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from './components/Sidebar.jsx';
+
+function App() {
+  return (
+    <main classsName="h-screen my-8">
+      <Sidebar />
+    </main>
+  );
+}
+
+export default App;
